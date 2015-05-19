6cda0e64aef05652be575bf79d495c773a588a95
Anton Leuski <anton@leuski.net>
2015-05-19 16:33:03 -0700

    Adding copyright info to source files.

--- Coiffeur/src/view/OptionsRowView.swift
+++ Coiffeur/src/view/OptionsRowView.swift
@@ -5,13 +5,28 @@
 //  Created by Anton Leuski on 4/16/15.
 //  Copyright (c) 2015 Anton Leuski. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//
 
 import Cocoa
 
 class ConfigCellView : NSTableCellView {
+  // prevent the state restoration mechanism to save/restore this view properties
+  override class func restorableStateKeyPaths() -> [AnyObject] { return [] }
 }
 
-class ConfigOptionCellView : NSTableCellView {
+class ConfigOptionCellView : ConfigCellView {
 	// will use this to shift the content of the cell appropriately
 	@IBOutlet weak var leftMargin : NSLayoutConstraint!
 }
@@ -29,7 +44,9 @@ extension ConfigNodeLocation {
 }
 
 class ConfigRowView : NSTableRowView {
-	
+  // prevent the state restoration mechanism to save/restore this view properties
+  override class func restorableStateKeyPaths() -> [AnyObject] { return [] }
+
 	@IBOutlet weak var leftMargin : NSLayoutConstraint!
 	@IBOutlet weak var textField : NSTextField!
 	
